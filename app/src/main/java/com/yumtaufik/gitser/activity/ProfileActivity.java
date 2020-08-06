90faec75ae2a854790b7071591e044e518f56f60
yumtaufikhidayat <taufik.hidayat@merahputih.id>
2020-08-06 18:10:02 +0700

    Updating profile activity.

--- app/src/main/java/com/yumtaufik/gitser/activity/ProfileActivity.java
+++ app/src/main/java/com/yumtaufik/gitser/activity/ProfileActivity.java
@@ -1,10 +1,15 @@
 package com.yumtaufik.gitser.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Window;
 
@@ -12,14 +17,22 @@ import com.yumtaufik.gitser.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    Toolbar toolbarProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        setInit();
+
         setWindowNotificationBg();
 
         setGetSupportActionBar();
+    }
+
+    private void setInit() {
+        toolbarProfile = findViewById(R.id.toolbarProfile);
     }
 
     //----Method to set notification bar----
@@ -36,6 +49,8 @@ public class ProfileActivity extends AppCompatActivity {
 
     //----Methods to set action bar----
     private void setGetSupportActionBar() {
+
+        setSupportActionBar(toolbarProfile);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle((Html.fromHtml("<font color=\"#DC143C\">" + getString(R.string.tvProfile) + "</font>")));
@@ -45,15 +60,30 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        super.onBackPressed();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                onBackPressed();
+                break;
+
+            case R.id.nav_settings:
+                //To settings layout
+                break;
+
+            case R.id.nav_language:
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
+                break;
         }
+
         return super.onOptionsItemSelected(item);
     }
     //----Ends-----
