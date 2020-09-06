a8530c71011aa1398f1f6b17c094bba476437a67
yumtaufikhidayat <taufik.hidayat@merahputih.id>
2020-09-06 17:52:30 +0700

    Adding some changes of settings activity.

--- app/src/main/java/com/yumtaufik/gitser/fragment/settings/SettingsFragment.java
+++ app/src/main/java/com/yumtaufik/gitser/fragment/settings/SettingsFragment.java
@@ -3,10 +3,14 @@ package com.yumtaufik.gitser.fragment.settings;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.yumtaufik.gitser.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    SwitchPreferenceCompat switchNotifications;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
