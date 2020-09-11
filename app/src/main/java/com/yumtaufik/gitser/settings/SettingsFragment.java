981ded0c5988b7445c22aa2f21f60640bfd5c108
yumtaufikhidayat <taufik.hidayat@merahputih.id>
2020-09-11 09:07:04 +0700

    Adding share icon and removing language preference from settings.

--- app/src/main/java/com/yumtaufik/gitser/settings/SettingsFragment.java
+++ app/src/main/java/com/yumtaufik/gitser/settings/SettingsFragment.java
@@ -3,26 +3,19 @@ package com.yumtaufik.gitser.settings;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.preference.CheckBoxPreference;
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.yumtaufik.gitser.R;
 import com.yumtaufik.gitser.service.AlarmReceiver;
 
-import es.dmoral.toasty.Toasty;
-
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    String KEY_DEFAULT_VALUE = "KEY_DEFAULT_VALUE";
-    String KEY_LIST_LANGUAGES, KEY_CHECKBOX_ALARM;
+    String KEY_CHECKBOX_ALARM;
 
     SharedPreferences sharedPreferences;
-
-    ListPreference listPreference;
     CheckBoxPreference checkBoxPreference;
 
     AlarmReceiver alarmReceiver;
@@ -36,16 +29,12 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         setPreferences();
 
         setCheckedAlarm();
-
-        setListOfLanguages();
     }
 
     private void setInit() {
 
-        KEY_LIST_LANGUAGES = getResources().getString(R.string.keyLanguage);
         KEY_CHECKBOX_ALARM = getResources().getString(R.string.keyNotifications);
 
-        listPreference = findPreference(KEY_LIST_LANGUAGES);
         checkBoxPreference = findPreference(KEY_CHECKBOX_ALARM);
 
         alarmReceiver = new AlarmReceiver();
@@ -74,39 +63,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         });
     }
 
-    private void setListOfLanguages() {
-
-        String languages = sharedPreferences.getString(KEY_LIST_LANGUAGES, KEY_DEFAULT_VALUE);
-
-        if ("1".equals(languages)) {
-            Log.i("langIndonesia", "setLangIndonesia: ");
-        } else if ("2".equals(languages)) {
-            Log.i("langEngUS", "setLangEngUS: ");
-        }
-
-        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object object) {
-
-                String selectedLanguage = (String) object;
-                if (preference.getKey().equals(KEY_LIST_LANGUAGES)) {
-                    switch (selectedLanguage) {
-                        case "1":
-                            //Insert code here to call Indonesia language
-                            Toasty.success(requireActivity(), "Indonesia selected", Toast.LENGTH_SHORT, true).show();
-                            break;
-
-                        case "2":
-                            //Insert code here to call English(US) language
-                            Toasty.success(requireActivity(), "English (US) selected", Toast.LENGTH_SHORT, true).show();
-                            break;
-                    }
-                }
-                return true;
-            }
-        });
-    }
-
     @Override
     public void onResume() {
         super.onResume();
@@ -122,7 +78,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
-        Log.i("KEY_LIST_LANGUAGES", "onSharedListLanguages: ");
         Log.i("KEY_CHECKBOX_ALARM", "onSharedCheckboxAlarm: ");
     }
 }
