44dbd72b965ad2dd202c9ef87a6cbd67d2cea7b7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-08 19:03:53 +0700

    Updating Github token.

--- app/src/main/java/com/yumtaufik/gitser/api/ApiInterface.java
+++ app/src/main/java/com/yumtaufik/gitser/api/ApiInterface.java
@@ -1,5 +1,6 @@
 package com.yumtaufik.gitser.api;
 
+import com.yumtaufik.gitser.BuildConfig;
 import com.yumtaufik.gitser.model.detail.DetailProfileResponse;
 import com.yumtaufik.gitser.model.followingfollowers.FollowingFollowersResponse;
 import com.yumtaufik.gitser.model.main.MainResponse;
@@ -16,31 +17,31 @@ import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    @Headers("Authorization: token " + Api.GITHUB_TOKEN)
+    @Headers("Authorization: token " + BuildConfig.GITHUB_TOKEN_PAT)
     @GET(Api.USERS_URL)
     Call<List<MainResponse>> getAllUsers();
 
-    @Headers("Authorization: token " + Api.GITHUB_TOKEN)
+    @Headers("Authorization: token " + BuildConfig.GITHUB_TOKEN_PAT)
     @GET(Api.SEARCH_USERS_URL)
     Call<SearchResponse> searchUserByUsername(@Query("q") String searchUser);
 
-    @Headers("Authorization: token " + Api.GITHUB_TOKEN)
+    @Headers("Authorization: token " + BuildConfig.GITHUB_TOKEN_PAT)
     @GET(Api.DETAIL_PROFILE_URL)
     Call<DetailProfileResponse> getDetailProfile(@Path("username") String username);
 
-    @Headers("Authorization: token " + Api.GITHUB_TOKEN)
+    @Headers("Authorization: token " + BuildConfig.GITHUB_TOKEN_PAT)
     @GET(Api.FOLLOWING_URL)
     Call<List<FollowingFollowersResponse>> getProfileFollowing(
             @Path(value = "username", encoded = true) String username
     );
 
-    @Headers("Authorization: token " + Api.GITHUB_TOKEN)
+    @Headers("Authorization: token " + BuildConfig.GITHUB_TOKEN_PAT)
     @GET(Api.FOLLOWERS_URL)
     Call<List<FollowingFollowersResponse>> getProfileFollowers(
             @Path(value = "username", encoded = true) String username
     );
 
-    @Headers("Authorization: token " + Api.GITHUB_TOKEN)
+    @Headers("Authorization: token " + BuildConfig.GITHUB_TOKEN_PAT)
     @GET(Api.REPOSITORY_URL)
     Call<List<RepositoryResponse>> getProfileRepository(
             @Path(value = "username", encoded = true) String username
